3ddd079d341cd10c6fcd1144a545b56c19886e31
ShyamSundhar1411 <clashwithchiefrpjyt@gmail.com>
2026-01-17 00:16:01 +0530

    chore(concurrency): initialized concurrency

--- concurrency/cvp.go
+++ concurrency/cvp.go
@@ -7,21 +7,21 @@ import (
 	"time"
 )
 
-func heavyTask(id int, wg *sync.WaitGroup){
+func heavyTask(id int, wg *sync.WaitGroup) {
 	defer wg.Done()
-	fmt.Printf("Task %d is starting\n",id)
-	for range 100_000_000{
+	fmt.Printf("Task %d is starting\n", id)
+	for range 100_000_000 {
 
 	}
 	fmt.Println(time.Now())
-	fmt.Printf("Task %d is finished \n",id)
+	fmt.Printf("Task %d is finished \n", id)
 }
 
-func main(){
+func main() {
 	numThreads := 4
 	runtime.GOMAXPROCS(numThreads)
 	var wg sync.WaitGroup
-	for i :=range numThreads{
+	for i := range numThreads {
 		wg.Add(1)
 		go heavyTask(i, &wg)
 	}
